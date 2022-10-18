fde8c742e8bd1ea9d48c84c3954bfc2d3027f4d2
tisha25 <tishadodiya7154@gmail.com>
2022-10-17 23:47:53 -0700

    Data Encapsulatin

---
+++ fundamental,1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+bool isunmeric(string str){
+	for(int i=0;i<str.length();i++)
+	if (isdisgit(str[i]==false);
+	return false /when one non numerice value is found; return false
+	return true;
+	}
+	int main(){
+		string str;
+		cout<<"enter a string :"
+		cin>>str;
+		if(isnumeric(str))
+		cout<<"this is number"<<endl;
+		
+	}
